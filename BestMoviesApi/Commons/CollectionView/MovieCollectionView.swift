b3e3793b172e763fb103b003be8a0a0dacfb9e58
Salih KIR <salihkir2020@gmail.com>
2022-07-02 16:48:51 +0300

    New Update

---
+++ BestMoviesApi/Commons/CollectionView/MovieCollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCollectionViiew.swift
+//  BestMoviesApi
+//
+//  Created by Salih KIR on 2.07.2022.
+//
+
+import UIKit
+
+class MovieCollectionView: UICollectionViewCell {
+    @IBOutlet weak var collectionImageView: UIImageView!
+    @IBOutlet weak var collectionPageController: UIPageControl!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
