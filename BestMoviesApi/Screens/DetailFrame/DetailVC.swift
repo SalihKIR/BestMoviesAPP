4622e881fc0dd39dbc24aecd059bd4bcdcf0646b
Salih KIR <salihkir2020@gmail.com>
2022-07-04 20:19:03 +0300

    New Designe

---
+++ BestMoviesApi/Screens/DetailFrame/DetailVC.swift
@@ -0,0 +1,12 @@
+//
+//  DetailVC.swift
+//  BestMoviesApi
+//
+//  Created by Salih KIR on 4.07.2022.
+//
+
+import Foundation
+
+class DetailVC{
+    
+}
---
+++ BestMoviesApi/Screens/DetailFrame/DetailVM.swift
@@ -0,0 +1,12 @@
+//
+//  DetailVM.swift
+//  BestMoviesApi
+//
+//  Created by Salih KIR on 4.07.2022.
+//
+
+import Foundation
+
+class DetailVM{
+    
+}
