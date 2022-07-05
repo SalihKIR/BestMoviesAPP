2ad0dcce5cdc1628dec678c7687a3ddf75a6a016
Salih KIR <salihkir2020@gmail.com>
2022-07-05 19:41:45 +0300

    New Updates

---
+++ BestMoviesApi/Extensions/StoryboardExtension.swift
@@ -0,0 +1,30 @@
+//
+//  StoryboardExtension.swift
+//  BestMoviesApi
+//
+//  Created by Salih KIR on 30.06.2022.
+//
+
+import Foundation
+import UIKit
+
+public enum StoryboardNames: String{
+    case main = "Main"
+    case detail = "Detail"
+}
+
+protocol StoryboardSettings {
+    var storyboardNames: StoryboardNames {get set}
+}
+
+public extension UIViewController {
+    
+    class func instantiate (storyboard: StoryboardNames = StoryboardNames.main , bundle: Bundle? = nil , identifier: String? = nil) -> Self{
+        let viewControllerIdentifier = identifier ?? String(describing: self)
+        let storyboard = UIStoryboard(name: storyboard.rawValue, bundle: bundle)
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: viewControllerIdentifier) as? Self else{preconditionFailure("Unable to instantiate view controller with identifier \(viewControllerIdentifier) as type \(type(of: self))")
+        }
+        return viewController
+    }
+}
+
