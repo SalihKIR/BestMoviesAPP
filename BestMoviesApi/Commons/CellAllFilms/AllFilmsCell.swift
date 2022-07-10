d67110a9b13e19d7f7de1065f8dc3e0808b46ed5
Salih KIR <salihkir2020@gmail.com>
2022-07-10 23:50:09 +0300

    New Update

---
+++ BestMoviesApi/Commons/CellAllFilms/AllFilmsCell.swift
@@ -0,0 +1,33 @@
+//
+//  AllFilmsCell.swift
+//  BestMoviesApi
+//
+//  Created by Salih KIR on 10.07.2022.
+//
+
+import UIKit
+
+class AllFilmsCell: UITableViewCell {
+    @IBOutlet weak var allFilmsImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+      
+    }
+    
+}
+extension AllFilmsCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    static var nibName: UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+}
--- BestMoviesApi/Commons/Network/MoveApi5.swift
+++ BestMoviesApi/Commons/Network/MoveApi5.swift
@@ -9,15 +9,15 @@ import Foundation
 protocol Movie5ApiProtocol {
     func movieDowlandMovie(url: String , completion: @escaping (RandomResponse? , String?) -> Void)
     func movieDowlandOneMovie(url: String , completion: @escaping (RandomoneMovie? , String?) -> Void)
-    func movieDowlandAllMovie(url: String , completion: @escaping (AllMovie? , String?) -> Void)
+    func movieDowlandAllMovieNew(url: String , completion: @escaping (AllMovieNew? , String?) -> Void)
+
 }
 
 
 extension API: Movie5ApiProtocol {
-    func movieDowlandAllMovie(url: String, completion: @escaping (AllMovie?, String?) -> Void) {
+    func movieDowlandAllMovieNew(url: String, completion: @escaping (AllMovieNew?, String?) -> Void) {
         network.getdata(url: url, completion: completion)
     }
-    
    
     func movieDowlandOneMovie(url: String, completion: @escaping (RandomoneMovie?, String?) -> Void) {
         network.getdata(url: url, completion: completion)
