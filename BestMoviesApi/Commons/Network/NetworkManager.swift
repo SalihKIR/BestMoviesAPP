bf5d616806c70058f8a1706d2377514f42c20c86
Salih KIR <salihkir2020@gmail.com>
2022-07-14 02:44:28 +0300

    New Updates

--- BestMoviesApi/Commons/Network/NetworkManager.swift
+++ BestMoviesApi/Commons/Network/NetworkManager.swift
@@ -13,23 +13,25 @@ final class NetworkManager{
     
     let sesions = URLSession.shared
     func getdata<T: Codable>(url: String ,completion: @escaping (T?,String?) -> Void){
-        let preparedURL = URL(string: url)
-        let task = sesions.dataTask(with: preparedURL!) { (data , res, error) in
-            DispatchQueue.main.async {
-                if let data = data {
-                    do {
-                        let responseObject = try JSONDecoder().decode(T.self, from: data)
-                        completion(responseObject, nil)
-                    }
-                    catch{
-                        print("Data Boş",error , error.localizedDescription)
+        if let preparedURL = URL(string: url) {
+            let task = sesions.dataTask(with: preparedURL) { (data , res, error) in
+                DispatchQueue.main.async {
+                    if let data = data {
+                        do {
+                            let responseObject = try JSONDecoder().decode(T.self, from: data)
+                            completion(responseObject, nil)
+                        }
+                        catch{
+                            print("Data Boş",error , error.localizedDescription)
+                        }
                     }
                 }
             }
+            task.resume()
+        }
+        
+        
+        func postData<T: Codable, K: Encodable>(url: String, params: K, completion: @escaping (T?) -> Void) {
+        }
     }
-    task.resume()
-    
-    func postData<T: Codable, K: Encodable>(url: String, params: K, completion: @escaping (T?) -> Void) {
-                }
-}
 }
